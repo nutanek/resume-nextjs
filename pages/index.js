0e691b48385db4dd1fc70ea2f2067029c6da450c
nutanek <nutcs@hotmail.com>
2017-12-07 14:35:46 +0700

    add loading page

--- pages/index.js
+++ pages/index.js
@@ -7,20 +7,44 @@ import Education from './../components/Education'
 import Skills from './../components/Skills'
 
 export default class Index extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            mounted: false
+        }
+    }
+
+    componentDidMount() {
+        this.setState({mounted:true})
+    }
+
     render() {
-        return (
-            <main>
-               <Header />
-               <About />
-               <Experiences />
-               <Education />
-               <Skills />
-               <style jsx global>{`
-                    body {
-                        font-family: 'Questrial', sans-serif;
-                    }
-                `}</style>
-            </main>
-        )
+        if (this.state.mounted) {
+            return (
+                <main>
+                   <Header />
+                   <About />
+                   <Experiences />
+                   <Education />
+                   <Skills />
+                   <style jsx global>{`
+                        body {
+                            font-family: 'Questrial', sans-serif;
+                        }
+                    `}</style>
+                </main>
+            )
+        } else {
+            return (
+                <section class="hero is-light is-fullheight has-text-centered">
+                <div class="hero-body">
+                  <div class="container">
+                    <h1 class="title">Loading...</h1>
+                  </div>
+                </div>
+              </section>
+            )
+        }
+        
     } 
 }
